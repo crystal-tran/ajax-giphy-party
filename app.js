fd5a6a7e8a6a670d44dc450b3230dc7cc5b0a7b4
Brandie Lucano <blucano.dev@gmail.com>
2023-11-14 17:46:25 -0800

    fetched dtata, working on appending

--- app.js
+++ app.js
@@ -7,11 +7,36 @@ async function getGiphy(evt) {
 
   const searchKeyword = $('#input-field').val();
 
-  const params = new URLSearchParams({ q: searchKeyword, api_key: MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym});
+  const params = new URLSearchParams({q: searchKeyword,
+    api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" });
+
   const response = await fetch(
-    `/https://api.giphy.com/v1/gifs/search?${params}`,
+    `https://api.giphy.com/v1/gifs/search?${params}`,
     { method: "GET" });
-  const text = await response.text();
-  console.log("resp=", response, "text=", text);
+  const data = await response.json();
+  console.log("resp=", response, "data=", data);
+
+  displayGif(data);
+
+
 }
+/** Accepts json data */
+function displayGif(results){
+  console.log(results)
+  //get length of results data
+  let numOfResults = results.data.length;
+  console.log(numOfResults)
+  //get random gif index
+  let randomGifIndex = Math.floor(Math.random() * numOfResults)
+  console.log(randomGifIndex)
+
+  let $gifContainer = $(".results").append("<div>")
+  let $gifImage = $()
+
+
+
+}
+
 console.log("Let's get this party started!");
+
+// api_key: MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
