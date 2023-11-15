15c2192c314504e9fa43e4f14cbb9059d938ca28
Crystal Tran <c.t.tran95@gmail.com>
2023-11-14 20:11:31 -0500

    created HTML and started on AJAX

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+"use strict";
+
+$("form").on("submit", getGiphy);
+
+async function getGiphy(evt) {
+  evt.preventDefault();
+
+  const searchKeyword = $('#input-field').val();
+
+  const params = new URLSearchParams({ q: searchKeyword, api_key: MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym});
+  const response = await fetch(
+    `/https://api.giphy.com/v1/gifs/search?${params}`,
+    { method: "GET" });
+  const text = await response.text();
+  console.log("resp=", response, "text=", text);
+}
 console.log("Let's get this party started!");
